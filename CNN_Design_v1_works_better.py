bec7c3bb47a235e4c25cd72c6e33d08c945ceb64
nivkopo <nivko8746@gmail.com>
2023-07-10 17:10:22 +0300

    two designs of CNN network

---
+++ CNN_Design_v1_works_better.py
@@ -0,0 +1,190 @@
+import matplotlib.pyplot as plt
+import seaborn as sns
+import keras
+from keras.models import Sequential
+from keras.layers import Dense, Conv2D, MaxPool2D, Flatten, Dropout, BatchNormalization
+from keras import regularizers
+from keras.preprocessing.image import ImageDataGenerator
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report, confusion_matrix
+from sklearn.utils import shuffle
+from keras.callbacks import ReduceLROnPlateau
+import tensorflow as tf
+import cv2
+import os
+import numpy as np  # linear algebra
+import pandas as pd  # data processing, CSV file I/O (e.g. pd.read_csv)
+from numpy.typing import NDArray
+from typing import Optional, Literal, Type, TypedDict
+
+labels = ['PNEUMONIA', 'NORMAL']
+IMG_SIZE = 150
+
+
+#Concatenate all images in the different folders to three lists: normal, bacterial and virus
+
+def get_training_data(data_dir) -> NDArray:
+    normal_data = []
+    bacterial_data = []
+    virus_data = []
+    for label in labels:
+        path = os.path.join(data_dir, label)
+        class_num = labels.index(label)
+        for img in os.listdir(path):
+            try:
+                img_arr = cv2.imread(os.path.join(path, img), cv2.IMREAD_GRAYSCALE)
+                # Reshaping images to preferred size
+                resized_arr = cv2.resize(img_arr, (IMG_SIZE, IMG_SIZE))
+                if label == 'NORMAL':
+                  normal_data.append([resized_arr, class_num])
+                elif label == 'PNEUMONIA':
+                  if "bacteria" in str(img):
+                    bacterial_data.append([resized_arr, class_num])
+                  elif "virus" in str(img):
+                    virus_data.append([resized_arr, class_num])
+            except Exception as e:
+                print(e)
+    return np.array(normal_data), np.array(bacterial_data), np.array(virus_data)
+
+
+# Includes both normal and pneumonia cases.
+normal_data, bacterial_data, virus_data = get_training_data('/content/drive/MyDrive/Deep Learning Project/chest_xray/')  # list of [image, label]
+
+
+
+from sklearn.utils import shuffle
+
+# Separate data and labels for each class
+X_normal = [data[0] for data in normal_data]
+y_normal = [data[1] for data in normal_data]
+
+X_bacterial = [data[0] for data in bacterial_data]
+y_bacterial = [data[1] for data in bacterial_data]
+
+X_virus = [data[0] for data in virus_data]
+y_virus = [data[1] for data in virus_data]
+
+
+# Perform train-test split for each class separately
+X_normal_train, X_normal_test, y_normal_train, y_normal_test = train_test_split(X_normal, y_normal, test_size=0.15, random_state=42)
+X_bacterial_train, X_bacterial_test, y_bacterial_train, y_bacterial_test = train_test_split(X_bacterial, y_bacterial, test_size=0.05, random_state=42)
+X_virus_train, X_virus_test, y_virus_train, y_virus_test = train_test_split(X_virus, y_virus, test_size=0.075, random_state=42)
+
+# Merge the data and labels for all classes
+X_train = np.concatenate((X_normal_train, X_bacterial_train, X_virus_train), axis=0)
+y_train = np.concatenate((y_normal_train, y_bacterial_train, y_virus_train), axis=0)
+X_test = np.concatenate((X_normal_test, X_bacterial_test, X_virus_test), axis=0)
+y_test = np.concatenate((y_normal_test, y_bacterial_test, y_virus_test), axis=0)
+
+# Shuffle the data
+X_train, y_train = shuffle(X_train, y_train, random_state=42)
+X_test, y_test = shuffle(X_test, y_test, random_state=42)
+
+# Normalize data
+X_train = X_train / 255.0
+X_test = X_test / 255.0
+
+# Reshape data for deep learning
+X_train = X_train.reshape(-1, IMG_SIZE, IMG_SIZE, 1)
+X_test = X_test.reshape(-1, IMG_SIZE, IMG_SIZE, 1)
+
+# Rest of the code for data augmentation and model training
+datagen = ImageDataGenerator(
+    featurewise_center=False,
+    samplewise_center=False,
+    featurewise_std_normalization=False,
+    samplewise_std_normalization=False,
+    zca_whitening=False,
+    rotation_range=30,
+    zoom_range=0.2,
+    width_shift_range=0.1,
+    height_shift_range=0.1,
+    horizontal_flip=True,
+    vertical_flip=False)
+
+datagen.fit(X_train)
+
+
+
+
+
+# --------- Building CNN network ----------#
+# num_of_test = 1
+# for i in range(num_of_test):
+model = Sequential()
+model.add(Conv2D(filters=32, kernel_size=(3, 3), padding='same',
+                activation='relu', input_shape=(IMG_SIZE, IMG_SIZE, 1)))
+model.add(MaxPool2D(pool_size=(2,2),strides=(2,2)))
+model.add(BatchNormalization())
+model.add(Dropout(0.2))
+
+model.add(Conv2D(filters=64, kernel_size=(3,3), padding="same", activation="relu"))
+model.add(Conv2D(filters=64, kernel_size=(3,3), padding="same", activation="relu"))
+model.add(MaxPool2D(pool_size=(2,2),strides=(2,2)))
+model.add(BatchNormalization())
+
+model.add(Conv2D(filters=128, kernel_size=(3,3), padding="same", activation="relu"))
+model.add(Conv2D(filters=128, kernel_size=(3,3), padding="same", activation="relu"))
+model.add(MaxPool2D(pool_size=(2,2),strides=(2,2)))
+model.add(BatchNormalization())
+model.add(Flatten())
+model.add(Dropout(0.2))
+
+model.add(Dense(128, activation='relu'))
+model.add(Dense(64, activation='relu'))
+model.add(Dense(units=1, activation='sigmoid'))
+
+
+model.compile(optimizer=keras.optimizers.Adam(learning_rate=0.0001),
+              loss='binary_crossentropy', metrics=['accuracy'])
+model.summary()
+
+# binary_crossentropy is chosen because we have a binary ouput, either sick or not.
+
+learning_rate_reduction = ReduceLROnPlateau(
+    monitor='val_loss', patience=2, verbose=1, factor=0.2, min_lr=0.00000001)
+# min_delta: threshold for measuring the new optimum, to only focus on
+#   significant changes.
+early_stop = tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=3,restore_best_weights=True)
+# early_stop = tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=3)
+
+history = model.fit(X_train, y_train, batch_size=16,
+                    epochs=30, validation_split=0.025, callbacks=[learning_rate_reduction,early_stop])
+
+# score = model.evaluate(X_test, y_test, verbose=0)
+
+# train_acc = history.history['accuracy']
+# val_acc = history.history['val_accuracy']
+# train_loss = history.history['loss']
+# val_loss = history.history['val_loss']
+
+
+test_loss, test_accuracy = model.evaluate(X_test, y_test, verbose=0)
+print("Test Loss:", test_loss)
+print("Test Accuracy:", test_accuracy)
+
+
+fig, ax = plt.subplots(1, 2)
+
+train_acc = history.history['accuracy']
+train_loss = history.history['loss']
+val_acc = history.history['val_accuracy']
+val_loss = history.history['val_loss']
+epochs = range(1, len(train_acc) + 1)
+
+fig.set_size_inches(20, 10)
+
+ax[0].plot(epochs, train_acc, 'bo-', label='Training Accuracy')
+ax[0].plot(epochs, val_acc, 'ro-', label='Validation Accuracy')
+ax[0].set_title('Training & Validation Accuracy')
+ax[0].legend()
+ax[0].set_xlabel("Epochs")
+ax[0].set_ylabel("Accuracy")
+
+ax[1].plot(epochs, train_loss, 'b-o', label='Training Loss')
+ax[1].plot(epochs, val_loss, 'r-o', label='Validation Loss')
+ax[1].set_title('Training & Validation Loss')
+ax[1].legend()
+ax[1].set_xlabel("Epochs")
+ax[1].set_ylabel("Loss")
+plt.show()
